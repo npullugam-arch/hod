e281704b51b7c67a1cf34a6780ba3e70a34edf25
nanda kishore <nandakishore24951a05l9@gmail.com>
2026-05-03 17:36:23 +0530

    x

--- src/main/java/com/college/hod/entity/Request.java
+++ src/main/java/com/college/hod/entity/Request.java
@@ -10,14 +10,7 @@ import com.college.hod.enums.RequestStatus;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(
-        name = "request",
-        indexes = {
-                @Index(name = "idx_request_status", columnList = "status"),
-                @Index(name = "idx_request_hod_id", columnList = "hod_id"),
-                @Index(name = "idx_request_student_id", columnList = "student_id")
-        }
-)
+@Table(name = "request")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Request {
 
